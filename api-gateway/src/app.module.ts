ce5ae5b711fc2e89213c3d82e7b6f14b96905c58
lfqcamargo <lfqcamargo@gmail.com>
2026-06-02 20:34:30 -0300

    first commit

---
+++ api-gateway/src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 100,
+    }]),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
